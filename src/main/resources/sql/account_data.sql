18363dba606a8c69ce920e8e58f20583beda798a
zhujiancom <eric87com@gmail.com>
2015-07-07 10:43:30 +0800

    account_data数据更新

---
+++ src/main/resources/sql/account_data.sql
@@ -0,0 +1,23 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.17 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('1','收银机','CASH_MACHINE','CASH','208271.00','RMB',NULL,'208271.00',NULL,NULL,NULL,'197');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('2','现金账户','CASH','CASH',NULL,'RMB',NULL,NULL,NULL,NULL,NULL,'0');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('3','储蓄卡账户','DEPOSIT','DEPOSIT',NULL,'RMB',NULL,NULL,NULL,NULL,NULL,'0');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('4','饿了么在线支付','ELE','VIRTUAL','123010.00','RMB',NULL,'123010.00',NULL,NULL,NULL,'195');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('5','饿了么活动补贴','FREE_ELE','VIRTUAL','10286.00','RMB',NULL,'10286.00',NULL,NULL,NULL,'148');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('6','美团账户','MT','VIRTUAL','21207.78','RMB',NULL,'21207.78',NULL,NULL,NULL,'158');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('7','大众点评团购账户','DPTG','VIRTUAL','49188.36','RMB',NULL,'49188.36',NULL,NULL,NULL,'163');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('8','美团外卖在线支付','MTWM','VIRTUAL','23671.00','RMB',NULL,'23671.00',NULL,NULL,NULL,'143');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('9','美团外卖活动补贴','FREE_MTWM','VIRTUAL','7054.00','RMB',NULL,'7054.00',NULL,NULL,NULL,'141');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('10','支付宝账户','TDD','VIRTUAL','11764.00','RMB',NULL,'11764.00',NULL,NULL,NULL,'124');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('11','免单账户','FREE','CASH','800.00','RMB',NULL,'800.00',NULL,NULL,NULL,'55');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('12','美团超级代金券在线支付','MT_SUPER','VIRTUAL','6450.94','RMB',NULL,'6450.94',NULL,NULL,NULL,'47');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('13','美团超级代金券立减10元','FREE_MT_SUPER','VIRTUAL','290.00','RMB',NULL,'290.00',NULL,NULL,NULL,'23');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('14','POS机账户','POS','VIRTUAL','22721.77','RMB',NULL,'22721.77',NULL,NULL,NULL,'85');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('15','大众点评闪惠账户','DPSH','VIRTUAL','10240.00','RMB',NULL,'10240.00',NULL,NULL,NULL,'55');
+insert into `bus_tb_account` (`accId`, `name`, `acc_no`, `type`, `balance`, `currency`, `description`, `earning_amount`, `expense_amount`, `is_parent`, `pid`, `version`) values('16','拉手网账户','LS','VIRTUAL',NULL,'RMB',NULL,NULL,NULL,NULL,NULL,'0');
