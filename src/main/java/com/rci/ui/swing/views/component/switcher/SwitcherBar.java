a5ce81c9c307f3a1c06b336c58abb6dbef3220a0
zhujiancom <erci87com@gmail.com>
2015-12-08 00:17:01 +0800

    dd

---
+++ src/main/java/com/rci/ui/swing/views/component/switcher/SwitcherBar.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.rci.ui.swing.views.component.switcher;
+
+import javax.swing.JPanel;
+
+/**
+ * remark (备注):
+ *
+ * @author zj
+ *	
+ * 项目名称：ReconciliationPro
+ *
+ * 类名称：SwitcherBar
+ *
+ * 包名称：com.rci.ui.swing.views.component.switcher
+ *
+ * Create Time: 2015年12月8日 上午12:03:47
+ *
+ */
+public class SwitcherBar extends JPanel {
+
+}
---
+++ src/main/java/com/rci/ui/swing/views/component/switcher/SwitcherElement.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.rci.ui.swing.views.component.switcher;
+
+import javax.swing.JLabel;
+
+/**
+ * remark (备注):
+ *
+ * @author zj
+ *	
+ * 项目名称：ReconciliationPro
+ *
+ * 类名称：SwitcherElement
+ *
+ * 包名称：com.rci.ui.swing.views.component.switcher
+ *
+ * Create Time: 2015年12月8日 上午12:04:07
+ *
+ */
+public class SwitcherElement extends JLabel {
+
+}
